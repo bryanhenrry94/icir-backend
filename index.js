004332857c69ee2f6d067392f7202e9ce21b8807
Bryan Navarrete <bryanhenrry94@gmail.com>
2020-06-26 07:41:14 -0500

    ok

--- index.js
+++ index.js
@@ -5,11 +5,14 @@ const routes = require('./src/routes/index');
 const cors = require('cors');
 require('./src/database');
 
+// sets port 3000 to default or unless otherwise specified in the environment
+app.set('port', process.env.PORT || 3000);
+
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/api', routes);
 
-app.listen(3700);
-console.log('Server on port ', 3700);
+app.listen(app.get('port'));
+console.log('Server on port ', app.get('port'));
