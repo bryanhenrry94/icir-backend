acbaeaeea1469a39898320c325fb8e600ffa8b2f
Bryan Navarrete <bryanhenrry94@gmail.com>
2020-06-27 22:31:02 -0500

    .

--- src/database.js
+++ src/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
-// const _URI = 'mongodb://admin:admin@cluster0-shard-00-00-yzt5m.mongodb.net:27017,cluster0-shard-00-01-yzt5m.mongodb.net:27017,cluster0-shard-00-02-yzt5m.mongodb.net:27017/icir?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority';
-const _URI = 'mongodb://localhost:27017/icir';
+const _URI = 'mongodb://admin:admin@cluster0-shard-00-00-yzt5m.mongodb.net:27017,cluster0-shard-00-01-yzt5m.mongodb.net:27017,cluster0-shard-00-02-yzt5m.mongodb.net:27017/icir?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority';
+//const _URI = 'mongodb://localhost:27017/icir';
 
 mongoose.connect(_URI, {
 }).then(db => console.log('Database in connected'))
